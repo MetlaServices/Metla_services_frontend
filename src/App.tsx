90e4c87232c778f05e3616e98ea3f2493036c51c
MetlaServices <servicesmetla@gmail.com>
2024-10-16 12:10:14 +0530

    Remove popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Nav from './Components/Navigation/Nav';
 import 'slick-carousel/slick/slick.css';
@@ -6,24 +6,14 @@ import 'slick-carousel/slick/slick-theme.css';
 import Routing from './Components/Routing/Routing';
 import Footer from './Components/Footer/Footer';
 import Chat from './Components/ChatBot/ChatBot';
-import PopupForm from './Components/PopupForm/PopUpForm'; // Make sure to import your PopupForm component
 
 function App() {
   const location = useLocation();
-  const [isPopupVisible, setIsPopupVisible] = useState(false);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [location]);
 
-  useEffect(() => {
-    // Open the popup form automatically when the component mounts
-    setIsPopupVisible(true);
-  }, []);
-
-  const handlePopupClose = () => {
-    setIsPopupVisible(false);
-  };
 
 
 
