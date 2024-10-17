591ed241cb668746c54ad0d995509a29acbf630f
MetlaServices <servicesmetla@gmail.com>
2024-10-17 12:19:34 +0530

    Popup

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import Nav from './Components/Navigation/Nav';
 import 'slick-carousel/slick/slick.css';
